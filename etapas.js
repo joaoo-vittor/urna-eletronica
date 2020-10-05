36d5bf0b3e92710297ad6699fb1a9878c57aa74e
João Vitor <joaoo.vittor007@gmail.com>
2020-10-05 00:13:35 -0300

    Alteração no arquivo estapas

---
+++ etapas.js
@@ -0,0 +1,89 @@
+let etapas = {
+  0: {
+    titulo: "vereador",
+    numeros: 5,
+    candidatos: {
+      27000: {
+        nome: "Renan Maracajá",
+        partido: "PSDC",
+        foto: "img-3.jpg",
+        legenda: "vereador"
+      },
+      55666: {
+        nome: "Pimentel Filho",
+        partido: "PSD",
+        foto: "img-2.jpg",
+        legenda: "vereador"
+      },
+      55555: {
+        nome: "Ivonete Ludgerio",
+        partido: "PSD",
+        foto: "img-6.jpg",
+        legenda: "vereador"
+      },
+      15333: {
+        nome: "Dr. Olimpio",
+        partido: "PMDB",
+        foto: "img-3.jpg",
+        legenda: "vereador"
+      },
+      65123: {
+        nome: "Napoleão Maracajá",
+        partido: "PC do B",
+        foto: "img-3.jpg",
+        legenda: "vereador"
+      },
+      40040: {
+        nome: "Anderson Maia",
+        partido: "PSB",
+        foto: "img-2.jpg",
+        legenda: "vereador"
+      }
+    }
+  },
+  1: {
+    titulo: "prefeito",
+    numeros: 2,
+    candidatos: {
+      12: {
+        nome: "Ana Cláudia Vital",
+        partido: "Podemos",
+        foto: "img-6.jpg",
+        legenda: "Prefeito",
+        vice: {
+          nome: "Wellignton Silva",
+          partido: "podemos",
+          foto: "img-3.jpg",
+          legenda: "Vice-Prefeito",
+          menor: true
+        }
+      },
+      15: {
+        nome: "Artur Bolinha",
+        partido: "PSL",
+        foto: "img-2.jpg",
+        legenda: "Prefeito",
+        vice: {
+          nome: " Annelise Meneguesso",
+          partido: "PSL",
+          foto: "img-1.jpg",
+          legenda: "Vice-Prefeito",
+          menor: true
+        }
+      },
+      45: {
+        nome: "Bruno Cunha Lima",
+        partido: "PSD",
+        foto: "img-2.jpg",
+        legenda: "Prefeito",
+        vice: {
+          nome: "Lucas Ribeiro",
+          partido: "PSC",
+          foto: "img-3.jpg",
+          legenda: "Vice-Prefeito",
+          menor: true
+        }
+      }
+    }
+  }
+}
