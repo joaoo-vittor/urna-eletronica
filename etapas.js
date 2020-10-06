83ba14b83ff9df926a6c57c3f5aa0ac3dbdcb7ce
João Vitor <joaoo.vittor007@gmail.com>
2020-10-06 20:03:16 -0300

    implementação das regras de negocio

--- etapas.js
+++ etapas.js
@@ -1,89 +1,89 @@
 let etapas = {
   0: {
-    titulo: "vereador",
+    titulo: "VEREADOR",
     numeros: 5,
-    candidatos: {
-      27000: {
+    candidatos: [
+      {
+        numero: '27000',
         nome: "Renan Maracajá",
         partido: "PSDC",
         foto: "img-3.jpg",
         legenda: "vereador"
       },
-      55666: {
+      {
+        numero: '55666',
         nome: "Pimentel Filho",
         partido: "PSD",
         foto: "img-2.jpg",
         legenda: "vereador"
       },
-      55555: {
+      {
+        numero: '55555',
         nome: "Ivonete Ludgerio",
         partido: "PSD",
         foto: "img-6.jpg",
         legenda: "vereador"
       },
-      15333: {
+      {
+        numero: '15333',
         nome: "Dr. Olimpio",
         partido: "PMDB",
         foto: "img-3.jpg",
         legenda: "vereador"
       },
-      65123: {
+      {
+        numero: '65123',
         nome: "Napoleão Maracajá",
         partido: "PC do B",
         foto: "img-3.jpg",
         legenda: "vereador"
       },
-      40040: {
+      {
+        numero: '40040',
         nome: "Anderson Maia",
         partido: "PSB",
         foto: "img-2.jpg",
         legenda: "vereador"
-      }
-    }
+      },
+    ]
   },
   1: {
-    titulo: "prefeito",
+    titulo: "PREFEITO",
     numeros: 2,
-    candidatos: {
-      12: {
+    candidatos: [
+      {
         nome: "Ana Cláudia Vital",
+        numero: '12',
         partido: "Podemos",
         foto: "img-6.jpg",
         legenda: "Prefeito",
-        vice: {
-          nome: "Wellignton Silva",
-          partido: "podemos",
-          foto: "img-3.jpg",
-          legenda: "Vice-Prefeito",
-          menor: true
-        }
+        vice: "Wellignton Silva",
+        vfoto: "img-3.jpg",
+        vlegenda: "Vice-Prefeito",
+        menor: true
       },
-      15: {
+      {
         nome: "Artur Bolinha",
+        numero: '15',
         partido: "PSL",
         foto: "img-2.jpg",
         legenda: "Prefeito",
-        vice: {
-          nome: " Annelise Meneguesso",
-          partido: "PSL",
-          foto: "img-1.jpg",
-          legenda: "Vice-Prefeito",
-          menor: true
-        }
+        vice: " Annelise Meneguesso",
+        vfoto: "img-1.jpg",
+        vlegenda: "Vice-Prefeito",
+        menor: true
       },
-      45: {
+      {
         nome: "Bruno Cunha Lima",
+        numero: '45',
         partido: "PSD",
         foto: "img-2.jpg",
         legenda: "Prefeito",
-        vice: {
-          nome: "Lucas Ribeiro",
-          partido: "PSC",
-          foto: "img-3.jpg",
-          legenda: "Vice-Prefeito",
-          menor: true
-        }
+        vice: "Lucas Ribeiro",
+        vfoto: "img-3.jpg",
+        vlegenda: "Vice-Prefeito",
+        menor: true
       }
-    }
+    ]
   }
 }
--- script.js
+++ script.js
@@ -1,2 +1,130 @@
+
+// variaveis de controle de interface
 let seuVotoPara = document.querySelector('.info-1-1 span')
 let cargo = document.querySelector('.info-1-2 span')
+let descricao = document.querySelector('.info-1-4')
+let aviso = document.querySelector('.info-2')
+let lateral = document.querySelector('.info-1-direita')
+let numeros = document.querySelector('.info-1-3')
+
+
+// variaveis de controle de ambiente
+let etapaAtual = 0
+let numero = ''
+let votoBranco = false
+
+
+function comecarEtapa() {
+  let etapa = etapas[etapaAtual]
+
+  let numeroHTML = ''
+  numero = ''
+  votoBranco = false
+
+  for (let i = 0; i < etapa.numeros; i++) {
+    if (i === 0) {
+      numeroHTML += '<div class="numero pisca"></div>'
+    } else {
+      numeroHTML += '<div class="numero"></div>'
+    }
+
+  }
+
+  seuVotoPara.style.display = 'none'
+  cargo.innerHTML = etapa.titulo
+  descricao.innerHTML = ''
+  aviso.style.display = 'none'
+  lateral.innerHTML = ''
+  numeros.innerHTML = numeroHTML
+
+}
+
+
+function atualizaInterface() {
+  let etapa = etapas[etapaAtual]
+  let candidato = etapa.candidatos.filter((item) => {
+    if (item.numero == numero) {
+      return true
+    } else {
+      return false
+    }
+  })
+
+  if (candidato.length > 0) {
+    candidato = candidato[0]
+    seuVotoPara.style.display = 'block'
+    aviso.style.display = 'block'
+    descricao.innerHTML = `Nome : ${candidato.nome} <br>Partido: ${candidato.partido}`
+    lateral.innerHTML = `<div class="info-1-img"><img src="img/${candidato.foto}" alt="imagem">${candidato.legenda}</div>`
+  } else {
+    seuVotoPara.style.display = 'block'
+    aviso.style.display = 'block'
+    descricao.innerHTML = '<div class="aviso-grande pisca">VOTO NULO</div>'
+  }
+
+}
+
+
+function clicou(n) {
+  let elNumero = document.querySelector('.numero.pisca')
+
+  if (elNumero != null) {
+    elNumero.innerHTML = n
+    numero = `${numero}${n}`
+
+    elNumero.classList.remove('pisca')
+
+    if (elNumero.nextElementSibling !== null) {
+      elNumero.nextElementSibling.classList.add('pisca')
+    } else {
+      atualizaInterface()
+    }
+
+  }
+
+}
+
+
+function branco() {
+  numero = ''
+  votoBranco = true
+
+  seuVotoPara.style.display = 'block'
+  aviso.style.display = 'block'
+  numeros.innerHTML = ''
+  descricao.innerHTML = '<div class="aviso-grande pisca">VOTO EM BRANCO</div>'
+  lateral.innerHTML = ''
+}
+
+
+function corrige() {
+  comecarEtapa()
+}
+
+
+function confirma() {
+  let etapa = etapas[etapaAtual]
+
+  let votoConfirmado = false
+
+  if (votoBranco === true) {
+    console.log('voto em branco confirmado')
+    votoConfirmado = true
+  } else if (numero.length === etapa.numeros) {
+    console.log('voto confirmado')
+    votoConfirmado = true
+  }
+
+  if (votoConfirmado) {
+    etapaAtual++
+    if (etapas[etapaAtual] !== undefined) {
+      comecarEtapa()
+    } else {
+      console.log('FIM')
+    }
+  }
+
+}
+
+
+comecarEtapa()
